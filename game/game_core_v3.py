fe97328617da2d6981fa49c1ac75c596a44ab558
feydh <victoriamoseeva7@gmail.com>
2024-12-05 21:48:55 +0500

    first commit

---
+++ game/game_core_v3.py
@@ -0,0 +1,49 @@
+import numpy as np
+
+def random_predict(number: int=1) -> int:
+    """Компьютер угадывает число
+
+    Args:
+        number (int, optional): Загаданное число. (По умолчанию 1)
+
+    Returns:
+        int: Количество попыток
+    """
+    count = 0
+    minimal, maximal = 1, 101
+    random_int = np.random.randint(minimal,maximal)
+    
+    while True:
+        count +=1
+        
+        if random_int > number:
+            maximal = random_int
+            random_int = np.random.randint(minimal,maximal)
+        elif random_int < number:
+            minimal = random_int
+            random_int = np.random.randint(minimal,maximal)
+        else:
+            return count
+
+def score_game(random_predict) -> int:
+    """Подсчитать среднее количество попыток за 1000 раз
+
+    Args:
+        random_predict (_type_): Функция угадывания
+
+    Returns:
+        int: Количество попыток в среднем
+    """
+    np.random.seed(1) #нужен ли он здесь?
+    counts = []
+    rand_ints = np.random.randint(1,101, size=(1000))
+    
+    for number in rand_ints:
+        counts.append(random_predict(number))
+        
+    score = int(np.mean(counts))
+    return score
+    
+print(score_game(random_predict))
+
+
