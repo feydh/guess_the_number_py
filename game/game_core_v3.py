ffca11db22fbf7d344899a25def4bb677043c061
feydh <victoriamoseeva7@gmail.com>
2024-12-05 22:42:36 +0500

    third commit

--- game/game_core_v3.py
+++ game/game_core_v3.py
@@ -9,19 +9,21 @@ def random_predict(number: int=1) -> int:
     Returns:
         int: Количество попыток
     """
-    count = 0
-    minimal, maximal = 1, 101
-    random_int = np.random.randint(minimal,maximal)
+    count = 0 
+    minimal, maximal = 1, 101 
+    random_int = np.random.randint(minimal,maximal) #генерируем рандомное число
     
     while True:
         count +=1
-        
+        # если рандомное число больше загаданного, то граница максимального числа изменяется и создается новое рандомное число
         if random_int > number:
             maximal = random_int
             random_int = np.random.randint(minimal,maximal)
+        # если рандомное число меньше загаданного, то граница минимального числа изменяется и создается новое рандомное число
         elif random_int < number:
             minimal = random_int
             random_int = np.random.randint(minimal,maximal)
+        # в противном случае возвращаем количество попыток
         else:
             return count
 
@@ -34,13 +36,13 @@ def score_game(random_predict) -> int:
     Returns:
         int: Количество попыток в среднем
     """
-    np.random.seed(1) #нужен ли он здесь?
+    np.random.seed(1)
     counts = []
-    rand_ints = np.random.randint(1,101, size=(1000))
-    
+    rand_ints = np.random.randint(1,101, size=(1000)) #генерируем рандомное число
+    # добавляем количество попыток в список
     for number in rand_ints:
         counts.append(random_predict(number))
-        
+    # высчитываем среднее число
     score = int(np.mean(counts))
     return score
     
